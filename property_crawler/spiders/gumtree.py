542860fb9a0bfe5de6e4f8012a0322a10dff5841
Tien Quang <nhtienquang@gmail.com>
2016-06-11 00:34:57 +0700

    gumtree

--- property_crawler/spiders/gumtree.py
+++ property_crawler/spiders/gumtree.py
@@ -9,35 +9,51 @@ from scrapy.loader import ItemLoader
 
 from property_crawler.items import ImageItems
 
-class IndoorImageCrawlSpider(CrawlSpider):
-    name = "Indoor-Image-Spider"
-    allowed_domains = ["people.csail.mit.edu"]
+class GumtreeImageCrawlSpider(CrawlSpider):
+    name = "Gumtree-Image-Spider"
+    allowed_domains = ["gumtree.com"]
     start_urls = [
-        'http://people.csail.mit.edu/brussell/research/LabelMe/Images/'
+        'https://www.gumtree.com/search?search_category=home-garden'
     ]
 
     custom_settings = {
         'ITEM_PIPELINES' :{'scrapy.pipelines.images.ImagesPipeline': 1},
-        'IMAGES_STORE':'./images'
+        'IMAGES_STORE':'./images/gumtree'
     }
 
     def parse(self, response):
-        image_directory_urls = response.xpath("//a/@href")
-        for url in image_directory_urls.extract():
-            if "indoor" in str(url).lower():
-                request = Request(urlparse.urljoin(self.start_urls[0],url), callback=self.parse_image_page)
+        next_page = response.xpath("//li[@class='pagination-next']//a/@href")
+        for url in next_page.extract():
+            yield Request(urlparse.urljoin('https://gumtree.com', url))
+
+        propertiesLinks = response.xpath("//a[@class='listing-link']/@href")
+        for url in propertiesLinks.extract():
+            if len(url) > 0:
+                print "------"
+                print urlparse.urljoin('https://gumtree.com', url)
+                request = Request(urlparse.urljoin('https://gumtree.com', url), callback=self.parse_image_page)
                 request.meta['directory'] = url
                 yield request
 
-
     def parse_image_page(self, response):
-        image_urls = response.xpath("//a/@href")
+        image_urls = response.xpath("//div[@id='vip-tabs-images']//li//img[@itemprop='image']/@src")
         l = ItemLoader(item=ImageItems(), response=response)
         items = []
         for url in image_urls.extract():
-            if "jpg" in url:
+            if "jpg" in url or "JPG" in url:
                 item = ImageItems()
-                item['image_urls'] = [urlparse.urljoin(response.url, url)]
+                item['image_urls'] = [url]
+                item['directory'] = response.meta['directory']
+                item['image_name'] = url
+                items.append(item)
+
+        image_urls = response.xpath("//div[@id='vip-tabs-images']//li//img[@itemprop='image']/@data-lazy")
+        l = ItemLoader(item=ImageItems(), response=response)
+        items = []
+        for url in image_urls.extract():
+            if "jpg" in url or "JPG" in url:
+                item = ImageItems()
+                item['image_urls'] = [url]
                 item['directory'] = response.meta['directory']
                 item['image_name'] = url
                 items.append(item)
@@ -47,6 +63,6 @@ process = CrawlerProcess({
     'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
 })
 
-process.crawl(IndoorImageCrawlSpider)
+process.crawl(GumtreeImageCrawlSpider)
 
 process.start()
