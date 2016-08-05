7f5b5235d30434cc7ee4bb7175d8ed9690e48e71
duyvk <duyvk142@gmail.com>
2016-08-05 19:22:33 +0200

    add exception for url extraction

--- property_crawler/spiders/gumtree-au.py
+++ property_crawler/spiders/gumtree-au.py
@@ -119,9 +119,11 @@ class GumtreeAuImageCrawlSpider(CrawlSpider):
         return x.replace(u'\xa0'," ")
 
     def extract_img_url(self, x):
-        m = re.search('(?<=large):\'(.*)\'}(.*)', x)
-        return m.group(1)
-
+        try:
+            m = re.search('(?<=large):\'(.*)\'}(.*)', x)
+            return m.group(1)
+        except AttributeError, TypeError:
+            print("Cannot extract from " + x)
 
 # process = CrawlerProcess({
 #     'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
