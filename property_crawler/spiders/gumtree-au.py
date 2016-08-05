5b2cb86d073945d1532e43d2c1a10fabe61b3948
duyvk <duyvk142@gmail.com>
2016-08-05 19:36:32 +0200

    add page limit

--- property_crawler/spiders/gumtree-au.py
+++ property_crawler/spiders/gumtree-au.py
@@ -85,8 +85,10 @@ class GumtreeAuImageCrawlSpider(CrawlSpider):
 
     def parse_page_list(self, response):
         next_pages = response.xpath("//a[@class='paginator__button paginator__button-next']//@href")
+
         for next_page in next_pages.extract():
-            yield Request(urlparse.urljoin('http://www.gumtree.com.au', next_page), callback=self.parse_page_list)
+            if "page-2" not in next_page:
+                yield Request(urlparse.urljoin('http://www.gumtree.com.au', next_page), callback=self.parse_page_list)
 
         item_urls = response.xpath("//div[@itemprop='offers']/div/a[@itemprop='url']/@href")
         for item_url in item_urls.extract():
