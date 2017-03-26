b9ec9c0bc6efc099cf422810adf053a6dc10cb71
kduy <duyvk142@gmail.com>
2017-03-26 03:48:57 +0200

    add queue

--- property_crawler/spiders/AWS_Reg.py
+++ property_crawler/spiders/AWS_Reg.py
@@ -2,43 +2,34 @@
 
 from argparse import ArgumentParser
 import boto3
-import json
-from os import environ
 import os
+from Queue import Queue
+from threading import Thread
+import time
+
+num_fetch_threads = 8
+enclosure_queue = Queue()
+
+PATH = "/Volumes/Data/WORKSPACE/git/bagiks/property_crawler/property_crawler/spiders/images/full_samsung"
+    CLEAN_PATH = "/Volumes/Data/WORKSPACE/git/bagiks/property_crawler/property_crawler/spiders/images/full_samsung_clean"
+DIRTY_PATH = "/Volumes/Data/WORKSPACE/git/bagiks/property_crawler/property_crawler/spiders/images/full_samsung_dirty"
+endpoint = 'https://rekognition.us-west-2.amazonaws.com'
+collection = 'MaxLabels'
+
 
 def get_client(endpoint):
-    # key_id = environ.get('ASIAIWGKCYTDHD5N5NIQ')
-    # secret_key = environ.get('73WaSuhDQ7bvGVpEVf5zTMH7GgCP9gIbeBEsVz8O')
-    # token = environ.get('FQoDYXdzEN///////////wEaDI35HO8OQQ0G5VC9KyKsAYJdLxJTqYSXDnDzhoTl+n86KsEgqv+dR2xQd1drX6k6av2ngJTcjG2h4HGJnjfH10u9N8SmWQzSlOeRgC14fDs8T7Pn6Pqi+0sNq/m435YkdZmRDsZo545U6X5qUWYgC+CCU7vfRkXH6dTltINN2SfpaioJC/t3CFIofSSmJSZxkd1Fb05DqWz65f7tyF1unBvHDPE5X03FrPffTeK1zs7M1JPx+H5sYxY86j4oytXbxgU=')
-    # if not key_id or not secret_key or not token:
-    #     raise Exception('Missing AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, or AWS_SESSION_TOKEN')
     client = boto3.client('rekognition', region_name='us-west-2', endpoint_url=endpoint, verify=False,
                           aws_access_key_id='ASIAIWGKCYTDHD5N5NIQ', aws_secret_access_key='73WaSuhDQ7bvGVpEVf5zTMH7GgCP9gIbeBEsVz8O', aws_session_token='FQoDYXdzEN///////////wEaDI35HO8OQQ0G5VC9KyKsAYJdLxJTqYSXDnDzhoTl+n86KsEgqv+dR2xQd1drX6k6av2ngJTcjG2h4HGJnjfH10u9N8SmWQzSlOeRgC14fDs8T7Pn6Pqi+0sNq/m435YkdZmRDsZo545U6X5qUWYgC+CCU7vfRkXH6dTltINN2SfpaioJC/t3CFIofSSmJSZxkd1Fb05DqWz65f7tyF1unBvHDPE5X03FrPffTeK1zs7M1JPx+H5sYxY86j4oytXbxgU=')
     return client
 
-def get_args():
-    parser = ArgumentParser(description='Call index faces')
-    parser.add_argument('-e', '--endpoint')
-    parser.add_argument('-i', '--image')
-    parser.add_argument('-c', '--collection')
-    return parser.parse_args()
-
-if __name__ == '__main__':
-    # args = get_args()
-    PATH = "/Volumes/Data/WORKSPACE/git/bagiks/property_crawler/property_crawler/spiders/images/full_iphone"
-    CLEAN_PATH = "/Volumes/Data/WORKSPACE/git/bagiks/property_crawler/property_crawler/spiders/images/full_iphone_clean"
-    DIRTY_PATH = "/Volumes/Data/WORKSPACE/git/bagiks/property_crawler/property_crawler/spiders/images/full_iphone_dirty"
-    endpoint = 'https://rekognition.us-west-2.amazonaws.com'
-    img = '/Volumes/Data/Dropbox (Personal)/phone/ebay da loc/phone_22 copy/ff437b51fa03c01add2d74e48c9d02d5bed9ea00.jpg'
-    collection = 'MaxLabels'
-    client = get_client(endpoint)
-    print client
-
-    with open('results.txt', 'a') as myfile:
-        for j in os.listdir(PATH):
-            if "jpg" not in j:
-                continue
-            img = os.path.join(PATH, j)
+def getLabels(index, q):
+    while True:
+        print '%s: Looking for the next enclosure' % index
+        img_path = q.get()
+        print '%s: Downloading:' % index, img_path
+        time.sleep(index + 2)
+        img = os.path.join(PATH, img_path)
+        with open('results.txt', 'a') as myfile:
             with open(img, 'rb') as image:
                 response = client.detect_labels(Image={'Bytes': image.read()})
                 z = []
@@ -46,18 +37,32 @@ if __name__ == '__main__':
                     z += str(i['Name']).lower().split(" ")
 
                 names = [str(i['Name']).lower() for i in response['Labels']]
-                myfile.write(j + "-" + ",".join(names) + "\n")
-
+                myfile.write(img_path + "-" + ",".join(names) + "\n")
 
                 if 'phone' in z or 'screen' in z or 'lcd' in z or 'tablet' in z:
-                    os.rename(img, os.path.join(CLEAN_PATH, j))
+                    os.rename(img, os.path.join(CLEAN_PATH, img_path))
                 else:
-                    os.rename(img, os.path.join(DIRTY_PATH, j))
-                    print j, z
+                    os.rename(img, os.path.join(DIRTY_PATH, img_path))
+                    print img_path, z
 
-    myfile.close()
+        myfile.close()
+        q.task_done()
 
-            # names = [ str(i['Name']).lower() for i in response['Labels']]
+client = get_client(endpoint)
 
-            # if "phone"  in " ".join(names) or 'tablet' in " ".join(names):
-            #     os.rename(img, os.path.join(CLEAN_PATH, j))
+
+for i in range(num_fetch_threads):
+    worker = Thread(target=getLabels, args=(i, enclosure_queue,))
+    worker.setDaemon(True)
+    worker.start()
+
+
+    # with open('results.txt', 'a') as myfile:
+for img in os.listdir(PATH):
+    if "jpg" not in img:
+        continue
+    enclosure_queue.put(img)
+
+print '*** Main thread waiting'
+enclosure_queue.join()
+print '*** Done'
